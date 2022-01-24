093c4f6858834bfd61195a842f838715239b3352
Ben Lenihan <benlenihan99@gmail.com>
2022-01-24 20:34:05 +0000

    Cube Moves now

---
+++ Assets/Scripts/MoveControl.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveControl : MonoBehaviour, IControllable
+{
+    private Vector3 dragPosition;
+    Renderer objectRenderer;
+    Color32 objectColour;
+    float initialDistance; 
+
+
+    public void moveTo(Vector3 pos)
+    {
+        Ray newPositionRay = Camera.main.ScreenPointToRay(pos);
+        Vector3 destination = newPositionRay.GetPoint(initialDistance);
+        dragPosition = destination;
+    }
+
+    public void objectDeselected()
+    {
+        objectRenderer.material.SetColor("_Color", objectColour);
+    }
+
+    public void objectSelected()
+    {
+        initialDistance = Vector3.Distance(Camera.main.transform.position, transform.position);
+        objectRenderer.material.SetColor("_Color", Color.yellow);
+    }
+
+    public void rotateObject(Vector3 v)
+    {
+        transform.Rotate(v, Space.World);
+    }
+
+    public void scale(float percentageChange)
+    {
+        Vector3 newScale = transform.localScale;
+        newScale += percentageChange * transform.localScale;
+
+        transform.localScale = newScale;
+    }
+
+    public void youveBeenTouched()
+    {
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dragPosition = transform.position;
+        objectRenderer = GetComponent<Renderer>();
+        objectColour = GetComponent<Renderer>().material.color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, dragPosition, 0.05f);
+    }
+}
